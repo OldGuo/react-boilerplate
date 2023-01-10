5334be7a31d32238affcf209eceec932649631f2
oldguo <young.guo@gmail.com>
2023-01-09 16:56:58 -0800

    *: 2023 refresh
    This repo hasn't been updated since 2018, so refresh cra using the
    command:
    
    `npx create-react-app my-app --template typescript`
    
    Notably, this adds ts and refreshes our npm package versions.

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import "bootstrap/dist/css/bootstrap.css";
-import "bootstrap/dist/css/bootstrap-theme.css";
-
-import React from "react";
-import ReactDOM from "react-dom";
-import "./styles/index.css";
-import App from "./App";
-
-ReactDOM.render(React.createElement(App), document.getElementById("root"));
